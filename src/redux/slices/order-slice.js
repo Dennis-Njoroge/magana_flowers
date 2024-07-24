142e2feac54fb665b6226c3627a792fd7d6dbe42
Dennis <dnjoroge@dynamicmobility.co.ke>
2024-07-24 16:35:56 +0300

    Added orders and payments reports

--- src/redux/slices/order-slice.js
+++ src/redux/slices/order-slice.js
@@ -1,5 +1,4 @@
 import {createSlice} from "@reduxjs/toolkit";
-import {toast} from "react-toastify";
 import {orderApis} from "@/services/order";
 import {setIsLoading} from "@/redux/slices/loading";
 
